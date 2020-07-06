5394763e4986465ae6f6410203907f225f74313f
gasparcho <gaspar_roiger@hotmail.com>
2020-07-06 16:18:33 -0300

    Updates

---
+++ precache-manifest.adb6b7f3ea9c4cfc145f8f055b094fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0164aca50c6dc44cc23ede143073b5",
+    "url": "/Email_Manager/index.html"
+  },
+  {
+    "revision": "6d4e3f48c502ea477849",
+    "url": "/Email_Manager/static/js/2.6a08b32a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Email_Manager/static/js/2.6a08b32a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f111e5148b2c061f3741",
+    "url": "/Email_Manager/static/js/main.1a7bc307.chunk.js"
+  },
+  {
+    "revision": "36a6b1f8f00d0b0159c2",
+    "url": "/Email_Manager/static/js/runtime-main.cc6cd266.js"
+  }
+]);
